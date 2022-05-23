2efad7fb22c6981aae4576b422e3f50e05c54ee1
ahammedmd852 <ahammedmd852@gmail.com>
2022-05-23 19:38:59 +0600

    put method added

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ async function run() {
     const bookingCollection = client
       .db("doctors_portal")
       .collection("bookings");
+    const userCollection = client.db("doctors_portal").collection("users");
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, options, updateDoc);
+      res.send(result);
+    });
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -32,37 +45,51 @@ async function run() {
       res.send(services);
     });
 
-    app.get('/available', async (req, res) => {
-      const date = req.query.date || 'May 22, 2022';
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 22, 2022";
 
       // step 1 : to get all services
       const services = await serviceCollection.find().toArray();
 
-      // step 2 : get the bookings of that day 
-      const query = {date: date};
+      // step 2 : get the bookings of that day
+      const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
 
-      // step 3 : for each service to booking that service 
-      services.forEach(service => {
-        const serviceBookings = bookings.filter( book => book.treatment.name === service.name);
-        const bookedSlots = serviceBookings.map(book => book.slot);
-        const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+      // step 3 : for each service to booking that service
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment.name === service.name
+        );
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
         service.slots = available;
-      })
+      });
 
       res.send(services);
+    });
 
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
     });
 
     app.post("/booking", async (req, res) => {
       const booking = req.body;
-      const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
       const exists = await bookingCollection.findOne(query);
-      if(exists){
-        return res.send({success: false, booking: exists});
+      if (exists) {
+        return res.send({ success: false, booking: exists });
       }
       const result = await bookingCollection.insertOne(booking);
-      return res.send({success: true, result});
+      return res.send({ success: true, result });
     });
   } finally {
     // await client.close();
